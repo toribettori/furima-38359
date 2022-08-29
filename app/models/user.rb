7dd7fa62fe9e5e002416d404857934564c3e03c7
toribettori <senngoku69@gmail.com>
2022-08-29 15:14:22 +0900

    新規登録のデータ保存

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname,         presence: true
+         validates :family_name_full, presence: true
+         validates :first_name_full,  presence: true
+         validates :family_name_harf, presence: true
+         validates :first_name_harf,  presence: true
+         validates :birth_day,        presence: true       
 end
