60c85d3d0d1ae8dd2a52053b6a28347605711d67
toribettori <senngoku69@gmail.com>
2022-08-31 15:42:14 +0900

    バリデーションの修正

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name {Faker::Name.last_name}
+    nickname {Faker::Name.last_name}
     email {Faker::Internet.free_email}
     password {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
