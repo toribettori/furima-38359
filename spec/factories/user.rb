27b33b59740ff25e88fe794fe39fc2d09d94a824
toribettori <senngoku69@gmail.com>
2022-08-31 16:36:19 +0900

    FactoryBotの設定

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.last_name}
+    nickname {Faker::Name.name}
     email {Faker::Internet.free_email}
     password {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    family_name_full { Gimei.last.kanji }
+    first_name_full { Gimei.first.kanji }
+    family_name_harf { Gimei.last.katakana }
+    first_name_harf { Gimei.first.katakana }
+    birth_day { "1983-03-03"}
   end
 end
