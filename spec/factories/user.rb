0dbb06a1f106343e26d28b5d0fa8d31df64506b0
toribettori <senngoku69@gmail.com>
2022-09-01 10:35:14 +0900

    FactoryBotのpasswordの修正

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname {Faker::Name.name}
     email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
+    password { "a1" + Faker::Internet.password(min_length: 4) }
     password_confirmation {password}
     family_name_full { Gimei.last.kanji }
     first_name_full { Gimei.first.kanji }
