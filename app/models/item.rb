36fc9266944d9f3c6f0620937b1af7da8d923989
toribettori <senngoku69@gmail.com>
2022-09-03 02:00:19 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+  belongs_to :category
+  belongs_to :delivery_charge
+  belongs_to :delivery_date
+  belongs_to :situation 
   has_one_attached :image
 end
