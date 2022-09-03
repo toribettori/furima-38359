aef330ab74399ad95a6e1365804ec458f3dacd62
toribettori <senngoku69@gmail.com>
2022-09-03 15:58:23 +0900

    商品の情報をテーブルに保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,16 @@
 class Item < ApplicationRecord
   validates :item_name,          presence: true
   validates :explanation,        presence: true
-  validates :price,              presence: true 
   validates :image,              presence: true
   validates :category_id,        numericality: { other_than: 1 , message: "can't be blank"}
   validates :situation_id,       numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :area_id,            numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_date_id,   numericality: { other_than: 1 , message: "can't be blank"}
-
+  with_options presence: true, format:{ with:/\A[0-9]+\z/} do
+    validates :price, numericality:{ only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                      presence: { message:"can't be blank"}
+  end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :area
