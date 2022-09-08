fbb2ab697f5aacfc2a64f98f5dbd9467c3948cf5
toribettori <senngoku69@gmail.com>
2022-09-08 15:10:23 +0900

    フォーム情報の保存

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,10 +1,29 @@
 class PurchasesController < ApplicationController
+  before_action :sold
 
   def index
     @purchase_address = PurchaseAddress.new
-    @item = Item.find(params[:item_id])
   end
   
   def create
-  end  
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index
+    end 
+  end
+  
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:post_code, :area_id, :municipality, :house_number, :building, :phone, :purchase_id).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+  def sold
+    @item = Item.find(params[:item_id])
+  end
+
+
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :area_id, :municipality, :house_number, :building, :phone, :user_id, :item_id, :token
+  attr_accessor :post_code, :area_id, :municipality, :house_number, :building, :phone, :user_id, :item_id
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -9,7 +9,6 @@ class PurchaseAddress
     validates :phone, format: {with: /\A[0-9]{10,11}\z/ }
     validates :user_id
     validates :item_id
-    validates :token
   end
 
   validates :area_id, numericality: {other_than: 1, message: "can't be blank"}
