515f94d8a5fe7071a4eea2e889bc9171affc0417
toribettori <senngoku69@gmail.com>
2022-09-08 19:16:03 +0900

    決済機能を実装とtokenにバリデーション

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -8,6 +8,7 @@ class PurchasesController < ApplicationController
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path
     else
@@ -18,12 +19,21 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_address).permit(:post_code, :area_id, :municipality, :house_number, :building, :phone, :purchase_id).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:purchase_address).permit(:post_code, :area_id, :municipality, :house_number, :building, :phone, :purchase_id).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
   def sold
     @item = Item.find(params[:item_id])
   end
+
+  def pay_item
+    Payjp.api_key = "sk_test_1700e996a3225a1dd4bcea75"
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: params[:token],
+        currency: 'jpy'
+      )
+  end   
 
 
 end
